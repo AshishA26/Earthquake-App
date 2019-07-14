1c42e27beb058582b623156e79e8605e23b0ad4b
Ashish <shinyflame007@gmail.com>
2019-07-14 17:18:26 -0400

    Added dummy earthquakes for testing

--- app/src/main/java/com/ashish/earthquake/EarthquakeMainActivity.java
+++ app/src/main/java/com/ashish/earthquake/EarthquakeMainActivity.java
@@ -36,6 +36,23 @@ public class EarthquakeMainActivity extends AppCompatActivity {
             mEarthquakeListFragment =
                     (EarthquakeListFragment)fm.findFragmentByTag(TAG_LIST_FRAGMENT);
         }
-
+        Date now = Calendar.getInstance().getTime();
+        List<Earthquake> dummyQuakes = new ArrayList<Earthquake>(0);
+        dummyQuakes.add(new Earthquake("0", now, "San Jose", null, 7.3, null));
+        dummyQuakes.add(new Earthquake("1", now, "LA", null, 6.5, null));
+        dummyQuakes.add(new Earthquake("2", now, "Africa", null, 10, null));
+        dummyQuakes.add(new Earthquake("3", now, "Manitoba", null, 1, null));
+        dummyQuakes.add(new Earthquake("4", now, "NYC", null, 3.4, null));
+        dummyQuakes.add(new Earthquake("5", now, "California", null, 5.6, null));
+        dummyQuakes.add(new Earthquake("6", now, "Palace", null, 7, null));
+        dummyQuakes.add(new Earthquake("7", now, "Place", null, 3.2, null));
+        dummyQuakes.add(new Earthquake("8", now, "America", null, 1.6, null));
+        dummyQuakes.add(new Earthquake("9", now, "Brazil", null, 2.1, null));
+        dummyQuakes.add(new Earthquake("10", now, "Argentina", null, 5.4, null));
+        dummyQuakes.add(new Earthquake("11", now, "Canada", null, 4.7, null));
+        dummyQuakes.add(new Earthquake("12", now, "Indonesia", null, 8, null));
+        dummyQuakes.add(new Earthquake("13", now, "Cali", null, 9, null));
+        dummyQuakes.add(new Earthquake("14", now, "Bali", null, 6, null));
+        mEarthquakeListFragment.setEarthquakes(dummyQuakes);
     }
 }
