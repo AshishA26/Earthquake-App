02dfa117ca2a79b9006ca30fda31f18befaf55b4
Ashish <shinyflame007@gmail.com>
2019-07-21 13:37:05 -0400

    Added data binding

--- app/src/main/java/com/ashish/earthquake/EarthquakeRecyclerViewAdapter.java
+++ app/src/main/java/com/ashish/earthquake/EarthquakeRecyclerViewAdapter.java
@@ -6,14 +6,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 public class EarthquakeRecyclerViewAdapter extends
         RecyclerView.Adapter<EarthquakeRecyclerViewAdapter.ViewHolder> {
     private final List<Earthquake> mEarthquakes;
-    public EarthquakeRecyclerViewAdapter(List<Earthquake> earthquakes ) {
+
+    private static final SimpleDateFormat TIME_FORMAT =
+            new SimpleDateFormat("HH:mm", Locale.US);
+    private static final NumberFormat MAGNITUDE_FORMAT =
+            new DecimalFormat("0.0");
+
+    public EarthquakeRecyclerViewAdapter(List<Earthquake> earthquakes) {
         mEarthquakes = earthquakes;
     }
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
@@ -21,28 +32,34 @@ public class EarthquakeRecyclerViewAdapter extends
                         parent, false);
         return new ViewHolder(view);
     }
+
     @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
-        holder.earthquake = mEarthquakes.get(position);
-        holder.detailsView.setText(mEarthquakes.get(position).toString());
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Earthquake earthquake = mEarthquakes.get(position);
+        holder.date.setText(TIME_FORMAT.format(earthquake.getDate()));
+        holder.details.setText(earthquake.getDetails());
+        holder.magnitude.setText(
+                MAGNITUDE_FORMAT.format(earthquake.getMagnitude()));
     }
+
     @Override
     public int getItemCount() {
         return mEarthquakes.size();
     }
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        public final View parentView;
-        public final TextView detailsView;
-        public Earthquake earthquake;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView date;
+        public final TextView details;
+        public final TextView magnitude;
+
         public ViewHolder(View view) {
             super(view);
-            parentView = view;
-            detailsView = (TextView)
-                    view.findViewById(R.id.list_item_earthquake_details);
+            date = (TextView) view.findViewById(R.id.date);
+            details = (TextView) view.findViewById(R.id.details);
+            magnitude = (TextView) view.findViewById(R.id.magnitude);
         }
-        @Override
-        public String toString() {
-            return super.toString() + " '" + detailsView.getText() + "'";
-        }
+
+    @Override
+    public String toString() { return super.toString() + " '" + details.getText() + "'"; }
     }
 }
